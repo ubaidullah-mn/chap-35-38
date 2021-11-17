bc72a5928041c19af95f32b996c8581464028c25
Ubaiduulah <ubaidullah5150@gmail.com>
2021-11-17 21:45:10 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,69 @@
+//        ques 1:
+//var datetime = new Date();
+//document.write("" + datetime);
+//       ques 2:
+//var name = prompt("Enter your first name");
+//var lastname = prompt("Enter your last name");
+//var fullname = name + lastname ;
+//if(user = fullname){
+ //       alert("hello " + fullname)
+//}
+//else{
+ //   alert("enter valid name")
+//}
+//    ques 3:
+//var num1 = +prompt("Enter Num1");
+//var num2 = +prompt("Enter Num2");
+//var sum = num1 + num2 ;
+//document.write("The sum of this number is " + sum )
+//     ques 4:
+/*var num1 = +prompt("Enter number 1")
+var opt = prompt("Enter opt")
+var num2 = +prompt("Enter number 2")
+function calc(num1 , opt , num2){
+    if(opt === "+"){
+        return num1 + num2
+    } else if(opt === "-"){
+        return num1 - num2
+    }else if(opt === "*"){
+        return num1 * num2
+    }else if(opt === "/"){
+        return num1 / num2
+    }else{
+        return "Enter correct opt"}
+ }       
+var value = calc(num1 , opt , num2)
+document.write(num1 + " " + opt + " " + num2 + " " + "=" + value)*/
+//       ques 5:
+//function bottle ( name ){
+ //   console.log(name)
+//}
+//bottle( "dom")
+//bottle( "dom")
+   //    ques 6:
+/*function foo(num1 , num2){
+    var a = num1 + num2
+    return a
+}
+var add = foo(50 , 100)
+console.log(add)*/
+//      ques 7:
+//var num1 = +prompt("Enter a start number for counting")
+//var num2 = +prompt("Enter a end number for counting")
+//document.write("Countings:" + "<br>")
+//for (var i = num1 ; i <=num2 ; i++){
+//  document.write(i + "<br>" )
+//}
+//        ques 8:
+/*var num1 = +prompt("Enter a rectangular height")
+var num2 = +prompt("Enter a rectangular width")
+function foo(num1 , num2){
+    var a = num1 * num2
+    return a
+}
+var multi = foo(num1 , num2)
+console.log(multi)*/
+
+
+
+
